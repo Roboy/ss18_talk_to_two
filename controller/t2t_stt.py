5d057873fbb7e1671fccc04dc845f35b5cad86df
ga84jul <jonas.hepp@tum.de>
2018-08-16 18:11:20 +0200

    hopefully fixed pyaudioanalysis

--- controller/t2t_stt.py
+++ controller/t2t_stt.py
@@ -7,13 +7,14 @@ BING_KEY = ''
 
 class T2t_stt:
     def __init__(self):
-        self.bing = bing_voice.BingVoice(BING_KEY)
+        #self.bing = bing_voice.BingVoice(BING_KEY)
         
     def get_text(self, audio):
         # recognize speech using Microsoft Bing Voice Recognition
         try:
             # pdb.set_trace()
-            text = self.bing.recognize(audio, language='en-US')
+            #text = self.bing.recognize(audio, language='en-US')
+            text = "Bing key is not set"
             # pdb.set_trace()
             print('Bing:' + text.encode('utf-8'))
             return text
