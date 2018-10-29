928ee88c85ed85e2c3601a4e26c6f3864a9e90d0
justDabuK <kevinjust87@gmail.com>
2018-10-29 19:50:05 +0100

    commented out the print thingy

--- SAM/led_visualizer.py
+++ SAM/led_visualizer.py
@@ -65,13 +65,13 @@ class LedVisualizer(Visualizer):
             if (len(speakers_for_vis) > 0):
                 speakers_for_vis = np.array(speakers_for_vis)
                 # ax.scatter(speakers_for_vis[:,1], speakers_for_vis[:,2], speakers_for_vis[:,3], s=speakers_for_vis[:,4])
-                print "------------------------------------"
-                print "speakers:"
-                print "x : ", speakers_for_vis[:, 1], "| y : ", speakers_for_vis[:, 2], "| z: ", speakers_for_vis, "| marker size: ", speakers_for_vis[:, 4]
-                print "------------------------------------"
-                """if len(speakers_for_vis[:, 1]) >= 4:
-    self.leds.visualize_da_4(led_by_angle(speakers_for_vis[1, 1]), led_by_angle(speakers_for_vis[2, 1]),
-                             led_by_angle(speakers_for_vis[3, 1]), led_by_angle(speakers_for_vis[4, 1]))"""
+                """print "------------------------------------"
+print "speakers:"
+print "x : ", speakers_for_vis[:, 1], "| y : ", speakers_for_vis[:, 2], "| z: ", speakers_for_vis, "| marker size: ", speakers_for_vis[:, 4]
+print "------------------------------------"
+if len(speakers_for_vis[:, 1]) >= 4:
+self.leds.visualize_da_4(led_by_angle(speakers_for_vis[1, 1]), led_by_angle(speakers_for_vis[2, 1]),
+             led_by_angle(speakers_for_vis[3, 1]), led_by_angle(speakers_for_vis[4, 1]))"""
                 for sp in speakers_for_vis:  # display the assigned id
                     if sp[0] > 0:
                         pixels[4 * led_by_angle(sp[1])] = color_array[vis_count][0]
@@ -83,15 +83,15 @@ class LedVisualizer(Visualizer):
             if (len(rec_for_vis) > 0):
                 rec_for_vis = np.array(rec_for_vis)
                 # ax.scatter(rec_for_vis[:,1], rec_for_vis[:,2], rec_for_vis[:,3], s=rec_for_vis[:,4])
-                print "------------------------------------"
-                print "recordings: "
-                print "x : ", rec_for_vis[:, 1], "| y : ", rec_for_vis[:,
-                                                                2], "| z: ", rec_for_vis, "| marker size: ", rec_for_vis[
-                                                                                                                  :, 4]
-                print "------------------------------------"
-                """if len(rec_for_vis[:, 1]) >= 4:
-                    self.leds.visualize_da_4(led_by_angle(rec_for_vis[0, 1]), led_by_angle(rec_for_vis[1, 1]),
-                                             led_by_angle(rec_for_vis[2, 1]), led_by_angle(rec_for_vis[3, 1]))"""
+                """print "------------------------------------"
+print "recordings: "
+print "x : ", rec_for_vis[:, 1], "| y : ", rec_for_vis[:,
+                                                2], "| z: ", rec_for_vis, "| marker size: ", rec_for_vis[
+                                                                                                  :, 4]
+print "------------------------------------"
+if len(rec_for_vis[:, 1]) >= 4:
+    self.leds.visualize_da_4(led_by_angle(rec_for_vis[0, 1]), led_by_angle(rec_for_vis[1, 1]),
+                             led_by_angle(rec_for_vis[2, 1]), led_by_angle(rec_for_vis[3, 1]))"""
                 for rec in rec_for_vis:  # display the assigned id
                     try:
                         pixels[4 * led_by_angle(rec[1])] = color_array[vis_count][0]
