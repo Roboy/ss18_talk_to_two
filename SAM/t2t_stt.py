f53f793a5a3fc0bd812136d6a59aba6ac997ea3d
justDabuK <kevinjust87@gmail.com>
2018-11-03 21:18:23 +0100

    changed the way of handling UnknownValueError

--- SAM/t2t_stt.py
+++ SAM/t2t_stt.py
@@ -17,31 +17,26 @@ class T2t_stt:
             pass
         
     def get_text(self, audio):
-        # recognize speech using Microsoft Bing Voice Recognition
-        try:
-            # pdb.set_trace()
-            if BING_KEY != '':
-                text = self.bing.recognize(audio, language='en-US')
-            else:
-                text = "Bing key is not set"
-            # pdb.set_trace()
-            output_text = 'Bing:' + text.encode('utf-8')
-            print output_text
-            self.pub.publish(output_text)
-            return text
-        except bing_voice.UnknownValueError:
-            traceback.print_exc()
-            output_text = "Microsoft Bing Voice Recognition could not understand audio"
-            print output_text
-            self.pub.publish(output_text)
-            return "Error"
-        except bing_voice.RequestError as e:
-            output_text = "Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e)
-            print output_text
-            self.pub.publish(output_text)
-            return "Error"
-        except Exception as e:
-            output_text = "Generic Bing Error", e
-            print output_text
-            self.pub.publish(output_text)
-            return "Error"
+        while 1:
+            # recognize speech using Microsoft Bing Voice Recognition
+            try:
+                # pdb.set_trace()
+                if BING_KEY != '':
+                    text = self.bing.recognize(audio, language='en-US')
+                else:
+                    text = "Bing key is not set"
+                # pdb.set_trace()
+                output_text = 'Bing:' + text.encode('utf-8')
+                self.pub.publish(output_text)
+                return text
+            except bing_voice.UnknownValueError:
+                output_text = "Microsoft Bing Voice Recognition could not understand audio, going to try it again"
+                self.pub.publish(output_text)
+            except bing_voice.RequestError as e:
+                output_text = "Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e)
+                self.pub.publish(output_text)
+                return "Error"
+            except Exception as e:
+                output_text = "Generic Bing Error", e
+                self.pub.publish(output_text)
+                return "Error"
