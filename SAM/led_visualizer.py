57730703f20750581c7940a6a7c4620bc5566b6f
justDabuK <kevinjust87@gmail.com>
2018-11-14 15:15:43 +0100

    adjusted range

--- SAM/led_visualizer.py
+++ SAM/led_visualizer.py
@@ -135,11 +135,11 @@ class LedVisualizer(Visualizer):
                         speaker_id.append(0)
                         record_leds.append(led_by_angle(np.arctan2(rec_for_vis[i, 1], rec_for_vis[i, 2]) * 180/np.pi))
                         # if 5 than 50 if 10 than + min / max
-                        bright = int(100 + ((200 - 100) / (9 - 7)) * (rec_for_vis[i, 5] - 7))
-                        if bright > 200:
-                            bright = 200
-                        if bright < 50:
-                            bright = 50
+                        bright = int(20 + ((250 - 20) / (9 - 7)) * (rec_for_vis[i, 5] - 7))
+                        if bright > 250:
+                            bright = 250
+                        if bright < 20:
+                            bright = 20
                         record_brightness.append(bright)
 
                     for r_led, bright in zip(record_leds, record_brightness):
